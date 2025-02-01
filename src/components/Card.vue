027e55c268bd95cd5c028d92150f8f14738ac84e
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2025-02-01 20:42:37 -0300

    add city weather

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,98 +1,62 @@
 <template>
-  <v-card class="card">
-    <h3>{{ data.date }}</h3>
-    <h3>{{ data.weekday }}</h3>
-    <strong>{{ data.description }}</strong>
+  <v-card :class="conditionBackground" class="card">
+    <span>{{ data.date }} - {{ data.weekday }}</span>
+    <h3>{{ data.description }}</h3>
 
-    <v-card-text class="py-5">
-      <v-col align="center" no-gutters>
-        <v-col class="text-h2"> 32&deg;C </v-col>
-
-        <v-col class="">
-          <v-icon color="error" icon="mdi-weather-hurricane" size="88"></v-icon>
-        </v-col>
+    <div>
+      <v-col class="d-flex flex-column justify-space-between align-center">
+        <span class="minMax">{{ data.min }}/{{ data.max }} &deg;C</span>
+        <v-img
+          :src="`https://assets.hgbrasil.com/weather/icons/conditions/${data.condition}.svg`"
+          contain
+          height="88"
+          width="88"
+        />
       </v-col>
-    </v-card-text>
+    </div>
+
+    <v-divider />
 
     <v-list class="bg-transparent">
-      <div class="d-flex py-3 justify-space-between">
+      <div class="d-flex flex-column justify-space-between">
         <v-list-item density="compact" prepend-icon="mdi-weather-windy">
           <v-list-item-subtitle>{{ data.wind_speedy }}</v-list-item-subtitle>
         </v-list-item>
 
         <v-list-item density="compact" prepend-icon="mdi-weather-pouring">
-          <v-list-item-subtitle
-            >{{ data.rain_probability }}%</v-list-item-subtitle
-          >
+          <v-list-item-subtitle>
+            {{ data.rain_probability }}%
+          </v-list-item-subtitle>
         </v-list-item>
       </div>
     </v-list>
-
-    <v-expand-transition>
-      <div v-if="expand">
-        <div class="py-2">
-          <v-slider
-            v-model="time"
-            :max="6"
-            :step="1"
-            :ticks="labels"
-            class="mx-4"
-            color="primary"
-            density="compact"
-            show-ticks="always"
-            thumb-size="10"
-            hide-details
-          ></v-slider>
-        </div>
-
-        <v-list class="bg-transparent">
-          <v-list-item
-            v-for="item in forecast"
-            :key="item.day"
-            :append-icon="item.icon"
-            :subtitle="item.temp"
-            :title="item.day"
-          >
-          </v-list-item>
-        </v-list>
-      </div>
-    </v-expand-transition>
-
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <v-btn
-        :text="!expand ? 'Mais' : 'Menos'"
-        @click="expand = !expand"
-      ></v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts" setup>
 import type { Forecast } from "@/types/weather";
-import { defineProps } from "vue";
+import { computed } from "vue";
 
-defineProps<{ data: Forecast }>();
+const props = defineProps<{ data: Forecast }>();
 
-import { ref } from "vue";
+const conditionBackground = computed(() => {
+  const conditionColors: Record<string, string> = {
+    storm: "stormy-bg",
+    snow: "snowy-bg",
+    hail: "hail-bg",
+    rain: "rainy-bg",
+    fog: "foggy-bg",
+    clear_day: "sunny-bg",
+    clear_night: "night-bg",
+    cloud: "cloudy-bg",
+    cloudly_day: "cloudy-day-bg",
+    cloudly_night: "cloudy-night-bg",
+    none_day: "error-day-bg",
+    none_night: "error-night-bg",
+  };
 
-const labels = ref({
-  0: "SU",
-  1: "MO",
-  2: "TU",
-  3: "WED",
-  4: "TH",
-  5: "FR",
-  6: "SA",
+  return conditionColors[props.data.condition] || "default";
 });
-const expand = ref(false);
-const time = ref(0);
-const forecast = ref([
-  { day: "Tuesday", icon: "mdi-white-balance-sunny", temp: "24°/12°" },
-  { day: "Wednesday", icon: "mdi-white-balance-sunny", temp: "22°/14°" },
-  { day: "Thursday", icon: "mdi-cloud", temp: "25°/15°" },
-]);
 </script>
 
 <style scoped>
@@ -103,5 +67,115 @@ const forecast = ref([
   min-width: 240px;
   min-height: 340px;
   text-align: center;
+}
+
+.minMax {
+  font-size: 30px;
+  padding: 0px;
+  margin: 0px;
+}
+
+.stormy-bg {
+  background: linear-gradient(
+    135deg,
+    #4a4a4a,
+    #000000
+  ); /* Cinza escuro para tempestade */
+}
+
+.snowy-bg {
+  background: linear-gradient(
+    135deg,
+    #d8efff,
+    #a6c5e8
+  ); /* Azul claro para neve */
+}
+
+.hail-bg {
+  background: linear-gradient(
+    135deg,
+    #a6c5e8,
+    #708090
+  ); /* Azul acinzentado para granizo */
+}
+
+.rainy-bg {
+  background: linear-gradient(
+    135deg,
+    #5f9ea0,
+    #4682b4
+  ); /* Azul esverdeado para chuva */
+}
+
+.foggy-bg {
+  background: linear-gradient(
+    135deg,
+    #c0c0c0,
+    #808080
+  ); /* Cinza para neblina */
+}
+
+.sunny-bg {
+  background: linear-gradient(
+    135deg,
+    #ffd700,
+    #ffa500
+  ); /* Amarelo para dia limpo */
+}
+
+.night-bg {
+  background: linear-gradient(
+    135deg,
+    #2c3e50,
+    #1a1a2e
+  ); /* Azul escuro para noite limpa */
+}
+
+.cloudy-bg {
+  background: linear-gradient(
+    135deg,
+    #b0c4de,
+    #778899
+  ); /* Azul claro para nublado */
+}
+
+.cloudy-day-bg {
+  background: linear-gradient(
+    135deg,
+    #d3d3d3,
+    #a9a9a9
+  ); /* Cinza claro para nublado de dia */
+}
+
+.cloudy-night-bg {
+  background: linear-gradient(
+    135deg,
+    #696969,
+    #2f4f4f
+  ); /* Cinza escuro para nublado de noite */
+}
+
+.error-day-bg {
+  background: linear-gradient(
+    135deg,
+    #ff8c00,
+    #ff4500
+  ); /* Laranja para erro de dia */
+}
+
+.error-night-bg {
+  background: linear-gradient(
+    135deg,
+    #8b0000,
+    #4b0082
+  ); /* Vermelho escuro para erro de noite */
+}
+
+.default-bg {
+  background: linear-gradient(
+    135deg,
+    #ffffff,
+    #dddddd
+  ); /* Branco com cinza claro como padrão */
 }
 </style>
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,16 +1,55 @@
 <template>
-  <div class="forecast-container" v-if="weather">
-    <Card v-for="(day, index) in weather.forecast" :key="index" :data="day" />
+  <div class="container" :class="conditionBackground">
+    <div class="city-container" v-if="weather">
+      <h1>{{ weather.city }}</h1>
+      <div class="wrapper-temp">
+        <!--
+       <DotLottieVue
+        style="height: 88px; width: 88px"
+        autoplay
+        loop
+        src="https://lottie.host/b919a743-a4e4-472a-9296-d2ff854dc7d8/nYNcPDwVCw.lottie"
+      />
+      -->
+        <v-img
+          :src="`https://assets.hgbrasil.com/weather/icons/conditions/${weather.condition_slug}.svg`"
+          contain
+          height="88"
+          width="88"
+        />
+        <v-col>
+          <span>{{ weather.description }}</span>
+          <h1>{{ weather.temp }}&deg;C</h1>
+        </v-col>
+      </div>
+
+      <div class="d-flex flex-column justify-space-between">
+        <v-list-item density="compact" prepend-icon="mdi-weather-windy">
+          <v-list-item-subtitle>{{ weather.wind_speedy }}</v-list-item-subtitle>
+        </v-list-item>
+
+        <v-list-item density="compact" prepend-icon="mdi-weather-pouring">
+          <v-list-item-subtitle>{{ weather.rain }}% </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item density="compact" prepend-icon="mdi-water">
+          <v-list-item-subtitle>{{ weather.humidity }}% </v-list-item-subtitle>
+        </v-list-item>
+      </div>
+    </div>
+    <div class="forecast-container" v-if="weather">
+      <Card v-for="(day, index) in weather.forecast" :key="index" :data="day" />
+    </div>
+    <p v-else-if="error">{{ error }}</p>
+    <p v-else>Carregando...</p>
   </div>
-  <p v-else-if="error">{{ error }}</p>
-  <p v-else>Carregando...</p>
 </template>
 
 <script setup lang="ts">
 import Card from "./Card.vue";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { getWeather } from "@/services/api.js";
 import type { WeatherResponse } from "@/types/weather";
+//import { DotLottieVue } from "@lottiefiles/dotlottie-vue";
 
 const weather = ref<WeatherResponse | null>(null);
 const error = ref(null);
@@ -40,17 +79,72 @@ onMounted(() => {
     error.value = "Geolocalização não suportada pelo navegador";
   }
 });
+
+const conditionBackground = computed(() => {
+  const conditionColors: Record<string, string> = {
+    dia: "day-bg",
+    noite: "night-bg",
+  };
+
+  return conditionColors[weather.value?.currently] || "default";
+});
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: start;
+  height: 100vh;
+  width: 100vw;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.wrapper-temp {
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 10px;
+}
+
+.city-container {
+  margin-left: 10px;
+}
+
+.night-bg {
+  background: linear-gradient(
+    135deg,
+    #6a1b9a,
+    /* Roxo escuro */ #8e24aa /* Lilás escuro */
+  ); /* Roxo e lilás para noite */
+}
+
+.day-bg {
+  background: linear-gradient(
+    135deg,
+    #f4a300,
+    /* Amarelo mais escuro */ #ff7f32 /* Laranja mais escuro */
+  ); /* Amarelo e laranja mais escuros para dia limpo */
+}
+
+.default {
+  background: linear-gradient(
+    135deg,
+    #4a4a4a,
+    #000000
+  ); /* Cinza escuro para tempestade */
+}
+
 .forecast-container {
   display: flex;
-  overflow-x: auto;
+  overflow-x: auto; /* Somente rolagem horizontal */
   gap: 10px;
   padding: 10px;
   white-space: nowrap;
   scrollbar-width: thin;
   scrollbar-color: #888 transparent;
+  max-width: 100%; /* Limita o tamanho do container */
 }
 
 .forecast-container::-webkit-scrollbar {
