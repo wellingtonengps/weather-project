11c550627004ebba00cc67a03fccdf31e88a49ef
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2025-02-02 19:30:43 -0300

    add urlBase

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,25 +3,16 @@ import type { WeatherResponse } from "@/types/weather";
 
 export const key = "f3cf07f7";
 
-//https://api.hgbrasil.com/weather?key=f3cf07f7&lat=-23.682&lon=-46.875&user_ip=remote
-
 const baseURL =
   import.meta.env.MODE === "development"
     ? "/api/weather" // Proxy em desenvolvimento
     : "https://api.hgbrasil.com/weather"; // URL real em produção
 
-console.log(baseURL);
-
-/*
-const api = axios.create({
-  baseURL,
-});*/
-
 export const getWeather = async (
   latitude: number,
   longitude: number
 ): Promise<WeatherResponse> => {
-  const response = await axios.get("/api/weather", {
+  const response = await axios.get(baseURL, {
     params: {
       key,
       lat: latitude,
