9de210fbb39d4a3d745c6a3b1dd1eaaaf7123071
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2025-02-02 17:16:42 -0300

    config proxy for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,18 @@ export const key = "f3cf07f7";
 
 //https://api.hgbrasil.com/weather?key=f3cf07f7&lat=-23.682&lon=-46.875&user_ip=remote
 
+/*
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "/api/weather" // Proxy em desenvolvimento
+    : "https://api.hgbrasil.com/weather"; // URL real em produção
+
+console.log(baseURL);
+
+const api = axios.create({
+  baseURL,
+});*/
+
 export const getWeather = async (
   latitude: number,
   longitude: number
